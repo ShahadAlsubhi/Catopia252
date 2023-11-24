18d7435b32b8f2f453e04ed82bb7b721d101e026
Shahad Alsubhi <ssalehalsubhi@stu.kau.edu.sa>
2023-11-24 04:12:33 +0300

    adding proxy classes

---
+++ src/Catopia252Proxy/AccountVerifier.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Catopia252Proxy;
+
+
+public interface AccountVerifier {
+    void verifyAccount(String username, String password);
+}
---
+++ src/Catopia252Proxy/RealAccountVerifier.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Catopia252Proxy;
+
+public class RealAccountVerifier implements AccountVerifier {
+    @Override
+    public void verifyAccount(String username, String password) {
+        // Actual implementation of account verification logic
+        // ...
+        System.out.println("Account verified successfully.");
+    }
+}
+
